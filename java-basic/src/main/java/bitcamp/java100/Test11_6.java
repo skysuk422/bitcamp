e9c4ec85200dbb6e4012ac1306a69ea88d1b2f67
skysuk422 <skysuk422@gmail.com>
2017-09-25 17:32:22 +0900

    '.'

---
+++ java-basic/src/main/java/bitcamp/java100/Test11_6.java
@@ -0,0 +1,25 @@
+package bitcamp.java100;
+  
+public class Test11_6{    
+    public static void main(String[] args) {
+        System.out.println(-21_4748_3648);
+        System.out.println(21_4748_3647);
+        
+        //System.out.println(-21_4748_3649);
+        //System.out.println(21_4748_3648);
+
+        System.out.println(-21_4748_3649l);
+        System.out.println(21_4748_3648l);
+
+        System.out.println(-21_4748_3649L);
+        System.out.println(21_4748_3648L);
+
+       //대부분 대문자 L사용
+
+       System.out.println(10);
+       System.out.println(10L);
+
+       System.out.println(-922_3372_0368_5477_5808L);
+       System.out.println(922_3372_0368_5477_5807L);
+    }
+    }
