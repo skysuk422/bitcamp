45f93c3b25bcfe109e537c6f7649184ffdaf9197
skysuk422 <skysuk422@gmail.com>
2017-10-27 17:14:11 +0900

    27

--- java-basic/src/main/java/bitcamp/java100/ch08/ex2/Test5.java
+++ java-basic/src/main/java/bitcamp/java100/ch08/ex2/Test5.java
@@ -3,26 +3,20 @@
 package bitcamp.java100.ch08.ex2;
 
 import java.io.File;
-import java.net.ServerSocket;
 
 public class Test5 {
 
     public static void main(String[] args) throws Exception {
         
-        Vehicle[] cars = new Vehicle[10];
-        cars[0] = new Car();
-        
-        cars[1] = new Boat();
-        cars[2] = new Truck();
-        cars[3] = new Sedan();
-        
-        Object[] objs = new Object[10];
-        objs[0] = new Car();
-        objs[1] = new String("hello");
-        objs[2] = new Integer(10);
-        objs[3] = new File("");
-        objs[4] = new StringBuffer();
-        objs[5] = new ServerSocket(9999);
+       Object[] arr = new Object[100];
+       
+       arr[0] = new String("Hello");
+       arr[1] = Integer.valueOf(100);
+       arr[2] = new File("./build.gradle");
+       
+       Car c1 = new Truck();
+       Car c2 = new Sedan();
+       
         
         
     }
--- java-project/src/main/java/java100/app/App.java
+++ java-project/src/main/java/java100/app/App.java
@@ -1,3 +1,4 @@
+package java100.app;
 import java.util.Scanner;
 
 public class App {
@@ -13,20 +14,24 @@ public class App {
     
     
     public static void main(String[] args) {
-      ScoreDao scoreDao = new ScoreDao();
+      ArrayList list = new ArrayList();
 //        ScoreDao scores = new ScoreDao();
         while (true) {
             Score score = new Score();
             score.input();
-            scoreDao.add(score);
+            list.add(score);
             
             
             if (!confirm("계속하시겠습니까? "))
                 break;
             
         }
-        for (int i = 0; i < scoreDao.size(); i++) {
-            scoreDao.get(i).print();
+        for (int i = 0; i < list.size(); i++) {
+//            list.get(i).print();
+//            Score s = (Score)list.get(i);
+//            s.print();
+            
+            ((Score)list.get(i)).print();
         }
         }
   }
